1ac874e4050bd166ac8f27345b80480d70109936
awdasdawdsadawdasd <79572158+awdasdawdsadawdasd@users.noreply.github.com>
2021-03-17 10:59:25 +0100

    had an assignment with Enums

---
+++ Directions/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Directions
+{
+    class Program
+    {
+        enum Directions
+        {
+            north = 1,
+            east,
+            south,
+            west
+        }
+
+        static void Main(string[] args)
+        {
+            //snap niet wrm ik deze aan moet maken
+            Directions playerDirection;
+            //gebruik 'm toch niet
+
+            Console.WriteLine("Please tell me which way you are headed: ");
+            Console.WriteLine("[1] north");
+            Console.WriteLine("[2] east");
+            Console.WriteLine("[3] south");
+            Console.WriteLine("[4] west\n");
+            int DirectionsID = int.Parse(Console.ReadLine());
+
+            playerDirection = (Directions)DirectionsID;
+
+            if (DirectionsID == 1)
+            {
+                Console.WriteLine("\nLooks like you are headed to the north.");
+            } else if(DirectionsID == 2)
+            {
+                Console.WriteLine("\nYou're headed to the east, aye?");
+            } else if(DirectionsID == 3)
+            {
+                Console.WriteLine("\nTo the south? Seriously?");
+            } else if(DirectionsID == 4)
+            {
+                Console.WriteLine("\nI never liked the west.");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("That answer is invalid!");
+                Console.WriteLine("\nGoodbye!");
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"\nWell then, I'll go with you to the {playerDirection}.");
+        }
+    }
+}
---
+++ Enumerated_Types/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enumerated_Types
+{
+    class Program
+    {
+        enum Menu
+        {
+            Start, // name = Start | Value = 0
+            Rules, // name = Rules | Value = 1
+            Quit   // name = Quit  | Value = 2
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> menu = new List<string> { "start", "rules", "quit" };
+
+            Menu selectedMenu;
+            selectedMenu = Menu.Start;
+            Console.WriteLine(selectedMenu);
+
+            selectedMenu = Menu.Rules;
+            Console.WriteLine(selectedMenu);
+            
+            Menu disabledMenu;
+            disabledMenu = Menu.Quit;
+
+            Console.WriteLine(disabledMenu);
+
+
+            // get value by casting enum name to int
+            int MenuID = (int)Menu.Rules;
+            Console.WriteLine($"Menu ID for rules is: {MenuID}.");
+
+            // get name from value
+            Menu MenuItem = (Menu)2;
+            Console.WriteLine($"Name that corosponds to Menu value 2 is: {MenuItem}.");
+
+            // loop through enums
+            foreach (string mItem in Enum.GetNames(typeof(Menu)))
+            {
+                Console.WriteLine("Menu item - " + mItem);
+            }
+
+            foreach (int mItem in Enum.GetValues(typeof(Menu)))
+            {
+                Menu name = (Menu)mItem;
+                Console.WriteLine("Menu item " + name + " has value - " + mItem);
+            }
+
+        }
+    }
+}
