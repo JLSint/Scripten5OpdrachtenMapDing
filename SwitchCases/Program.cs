5e5addc961ef74856a6a5a646de471e43023d354
awdasdawdsadawdasd <79572158+awdasdawdsadawdasd@users.noreply.github.com>
2021-04-14 13:56:30 +0200

    idk

---
+++ SwitchCases/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SwitchCases
+{
+    class Program
+    {
+        #region DaysOfTheWeek
+        enum WeekDays { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+        #endregion
+        static void Main(string[] args)
+        {
+            WeekDays today;
+
+            today = WeekDays.Thursday;
+
+            switch (WeekDays)
+            {
+                case WeekDays.Monday:
+                    {
+                        Console.WriteLine("Today, it's monday");
+                        break;
+                    }
+                case WeekDays.Tuesday:
+                    {
+
+                        break;
+                    }
+                case WeekDays.Wednesday:
+                    {
+
+                        break;
+                    }
+                case WeekDays.Thursday:
+                    {
+
+                        break;
+                    }
+                case WeekDays.Friday:
+                    {
+
+                        break;
+                    }
+                case WeekDays.Saturday:
+                    {
+
+                        break;
+                    }
+                case WeekDays.Sunday:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
